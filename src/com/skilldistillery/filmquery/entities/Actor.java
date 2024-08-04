a2575608edaf4e01511b084417e5cc9a5c644608
Yola Rivers <yolarivers@gmail.com>
2024-08-04 17:33:39 -0500

    Implemented a user interface with a menu that allows the user to: Look up a film by its ID, Look up a film by a search keyword and Exit the application

--- src/com/skilldistillery/filmquery/entities/Actor.java
+++ src/com/skilldistillery/filmquery/entities/Actor.java
@@ -10,25 +10,51 @@ public class Actor {
  
   public Actor() {}
 
+ 
   public Actor(int id, String firstName, String lastName) {
     this.id = id;
     this.firstName = firstName;
     this.lastName = lastName;
   }
 
- 
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
  
   @Override
   public String toString() {
     return "Actor [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + "]";
   }
 
-
+ 
   @Override
   public int hashCode() {
     return Objects.hash(id);
   }
 
+ 
   @Override
   public boolean equals(Object obj) {
     if (this == obj)
--- src/com/skilldistillery/filmquery/entities/Film.java
+++ src/com/skilldistillery/filmquery/entities/Film.java
@@ -17,7 +17,7 @@ public class Film {
   private String specialFeatures;
   private List<Actor> actors;
 
-  // Constructors, getters, setters, toString, equals, and hashCode
+  public Film() {}
 
   public Film(int id, String title, String description, short releaseYear, int languageId, 
               int rentalDuration, double rentalRate, int length, double replacementCost, 
@@ -35,7 +35,93 @@ public class Film {
     this.specialFeatures = specialFeatures;
   }
 
-  // Getters and setters for all fields, including actors
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public short getReleaseYear() {
+    return releaseYear;
+  }
+
+  public void setReleaseYear(short releaseYear) {
+    this.releaseYear = releaseYear;
+  }
+
+  public int getLanguageId() {
+    return languageId;
+  }
+
+  public void setLanguageId(int languageId) {
+    this.languageId = languageId;
+  }
+
+  public int getRentalDuration() {
+    return rentalDuration;
+  }
+
+  public void setRentalDuration(int rentalDuration) {
+    this.rentalDuration = rentalDuration;
+  }
+
+  public double getRentalRate() {
+    return rentalRate;
+  }
+
+  public void setRentalRate(double rentalRate) {
+    this.rentalRate = rentalRate;
+  }
+
+  public int getLength() {
+    return length;
+  }
+
+  public void setLength(int length) {
+    this.length = length;
+  }
+
+  public double getReplacementCost() {
+    return replacementCost;
+  }
+
+  public void setReplacementCost(double replacementCost) {
+    this.replacementCost = replacementCost;
+  }
+
+  public String getRating() {
+    return rating;
+  }
+
+  public void setRating(String rating) {
+    this.rating = rating;
+  }
+
+  public String getSpecialFeatures() {
+    return specialFeatures;
+  }
+
+  public void setSpecialFeatures(String specialFeatures) {
+    this.specialFeatures = specialFeatures;
+  }
 
   public List<Actor> getActors() {
     return actors;
@@ -50,28 +136,21 @@ public class Film {
     return "Film [id=" + id + ", title=" + title + ", description=" + description + ", releaseYear=" + releaseYear
         + ", languageId=" + languageId + ", rentalDuration=" + rentalDuration + ", rentalRate=" + rentalRate
         + ", length=" + length + ", replacementCost=" + replacementCost + ", rating=" + rating
-        + ", specialFeatures=" + specialFeatures + "]";
+        + ", specialFeatures=" + specialFeatures + ", actors=" + actors + "]";
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(id, title, description, releaseYear, languageId, rentalDuration, rentalRate, length, replacementCost, rating, specialFeatures, actors);
+    return Objects.hash(id);
   }
 
   @Override
   public boolean equals(Object obj) {
     if (this == obj)
       return true;
-    if (obj == null)
-      return false;
-    if (getClass() != obj.getClass())
+    if (obj == null || getClass() != obj.getClass())
       return false;
     Film other = (Film) obj;
-    return id == other.id && title.equals(other.title) && description.equals(other.description) 
-        && releaseYear == other.releaseYear && languageId == other.languageId 
-        && rentalDuration == other.rentalDuration && Double.compare(other.rentalRate, rentalRate) == 0 
-        && length == other.length && Double.compare(other.replacementCost, replacementCost) == 0 
-        && rating.equals(other.rating) && specialFeatures.equals(other.specialFeatures)
-        && Objects.equals(actors, other.actors);
+    return id == other.id;
   }
 }
